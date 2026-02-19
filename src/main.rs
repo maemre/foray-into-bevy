2d4ae6ed4e7f81a8f46fa532d07223a550e40ca4
Mehmet Emre <maemre2@gmail.com>
2026-02-18 22:17:35 -0800

    log plugin example

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
-use bevy::prelude::*;
+use bevy::{
+    log::{Level, LogPlugin},
+    prelude::*,
+};
 use foray_into_bevy::{pipes::PipePlugin, *};
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(LogPlugin {
+            filter: "foray_into_bevy::pipes=info".into(),
+            level: Level::ERROR,
+            ..default()
+        }))
         .add_plugins(PipePlugin)
         .add_systems(Startup, setup)
         .add_systems(FixedUpdate, (gravity, check_out_of_bounds))
